f5efa0e35bccb1e7bfc13c7a5a5b4017f59b707f
Peter Kokot <peterkokot@gmail.com>
2014-10-24 12:06:52 +0200

    contribute section improved

---
+++ assets/js/contributors.js
@@ -0,0 +1,52 @@
+(function ($) {
+    // Load contributors
+    var $contributors = $('#contributors');
+    if ( $contributors.length ) {
+        var fail = function () {
+            $contributors.html('<p><a href="https://github.com/wwphp-fb/php-faq/graphs/contributors">Awesome contributors on GitHub</a></p>');
+        };
+        $.ajax({
+            cache: false,
+            dataType: 'jsonp',
+            timeout: 3000,
+            type: 'GET',
+            url: 'https://api.github.com/repos/wwphp-fb/wwphp-fb.github.io/contributors?per_page=100'
+        }).done(function (data) {
+            // php-faq repository
+            $.ajax({
+                cache: false,
+                dataType: 'jsonp',
+                timeout: 3000,
+                type: 'GET',
+                url: 'https://api.github.com/repos/wwphp-fb/php-faq/contributors?per_page=100'
+            }).done(function (data_2) {
+                if ( data.data && data.data.length && data_2.data && data_2.data.length ) {
+                    var $ul = $('<ul></ul>'), dataLength = data.data.length, contributors = new Array(), dataLength_2 = data_2.data.length;
+                    for ( var i = 0; i < dataLength; i++ ) {
+                        contributors[i] = new Array(data.data[i].login, data.data[i].avatar_url, data.data[i].contributions);
+                    }
+                    var contributorsLength = contributors.length;
+                    for ( var i = 0; i < dataLength_2; i++ ) {
+                        cntn = false;
+                        for ( var j = 0; j < contributorsLength; j++ ) {
+                            if(contributors[j][0] == data_2.data[i].login) {
+                                contributors[j][2] += data_2.data[i].contributions;
+                                cntn = true;
+                            }
+                        }
+                        if (cntn) continue;
+                        contributors.push(new Array(data_2.data[i].login, data_2.data[i].avatar_url, data_2.data[i].contributions));
+                    }
+                    for ( var i = 0; i < contributors.length; i++ ) {
+                        $ul.append(['<div style="float: left; margin-right: 20px; margin-bottom: 20px;"><a href="https://github.com/', contributors[i][0], '" target="_blank"><img src="', contributors[i][1], '" width="75" /> ', contributors[i][0], '</a> (commits: ', contributors[i][2], ')</div>'].join(''));
+                    }
+                    console.log(contributors);
+                    $ul.append('<div style="clear:both;"></div>');
+                    $contributors.html($ul);
+                } else {
+                    fail();
+                }
+            }).fail(fail);
+        }).fail(fail);
+    }
+})(jQuery);
---
+++ contribute.md
@@ -0,0 +1,36 @@
+---
+layout: default
+title: "Contribute to wwphp-fb.github.io and FAQ"
+origin: "contribute"
+og_image: "assets/img/elephpant.png"
+---
+<div class="row" style="padding-top:8px">
+    <div class="large-12 columns">
+
+    <div style="text-align:center;">
+        <img src="/assets/img/elephpant.png" alt="elePHPant">
+    </div>
+
+    <h1 style="font-family:Audiowide;text-align:center">Contribute</h1>
+
+    <p>Contributing to open source projects can and should be first of all fun but it requires also lots of technical skills, patience,
+    ability to work with others, constant adaptations and many commitments as well.</p>
+
+    <p>Helping others and sharing PHP content in our Facebook group is definitely respectful and awesome contribution to the PHP community and we cannot properly
+    thank each and everyone that is dedicating their time and knowledge in helping in this Facebook group, so we have made this special section where
+    your contribution can get noted on this page as well.</p>
+
+    <h4>PHP FAQ</h4>
+
+    <p>Another important section of this group is PHP-FAQ. What you can do to help out?</p>
+
+    <p>You can go through the <a href="https://github.com/wwphp-fb/php-faq/issues">open issues</a> on GitHub directly or go through the
+    <a href="/faq">list of frequently asked questions</a> and check if there are missing information, missing questions that should be pointed
+    or fix typos.</a>
+
+    <h4>Contributors</h4>
+
+    <p>Special credits for contributions to this page goes to these awesome people:</p>
+
+    <div id="contributors"></div>
+</div>
