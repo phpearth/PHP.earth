fa95d8cd3051d2d4e3c7f119af48f323ec209b35
Peter Kokot <peterkokot@gmail.com>
2014-11-11 19:47:19 +0100

    micro events info added to contribute page, contributors list html fixed

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -21,7 +21,7 @@
                 url: 'https://api.github.com/repos/wwphp-fb/php-faq/contributors?per_page=100'
             }).done(function (data_2) {
                 if ( data.data && data.data.length && data_2.data && data_2.data.length ) {
-                    var $ul = $('<ul></ul>'), dataLength = data.data.length, contributors = new Array(), dataLength_2 = data_2.data.length;
+                    var $ul = $('<ul class="medium-block-grid-3"></ul>'), dataLength = data.data.length, contributors = new Array(), dataLength_2 = data_2.data.length;
                     for ( var i = 0; i < dataLength; i++ ) {
                         contributors[i] = new Array(data.data[i].login, data.data[i].avatar_url, data.data[i].contributions);
                     }
@@ -38,9 +38,8 @@
                         contributors.push(new Array(data_2.data[i].login, data_2.data[i].avatar_url, data_2.data[i].contributions));
                     }
                     for ( var i = 0; i < contributors.length; i++ ) {
-                        $ul.append(['<div style="float: left; margin-right: 20px; margin-bottom: 20px;"><a href="https://github.com/', contributors[i][0], '" target="_blank"><img src="', contributors[i][1], '" width="75" /> ', contributors[i][0], '</a> (commits: ', contributors[i][2], ')</div>'].join(''));
+                        $ul.append(['<li><a href="https://github.com/', contributors[i][0], '" target="_blank"><img src="', contributors[i][1], '" width="75" /> ', contributors[i][0], '</a> (commits: ', contributors[i][2], ')</li>'].join(''));
                     }
-                    $ul.append('<div style="clear:both;"></div>');
                     $contributors.html($ul);
                 } else {
                     fail();
