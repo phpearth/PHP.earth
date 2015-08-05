ff2f904e76d2e9597f5a6fb77e524c1d7c779b75
Peter Kokot <peterkokot@gmail.com>
2015-08-05 03:20:41 +0200

    Move rules generator to build script, bump rules, update indexdata.json file

--- build.php
+++ build.php
@@ -84,3 +84,23 @@ $indexData = ["entries" => $index];
 
 file_put_contents('./faq-facebook-document.html', $data, LOCK_EX);
 file_put_contents('./assets/js/lunr/indexdata.json', json_encode($indexData), LOCK_EX);
+
+
+// Parse rules
+
+// parse rules.yml file
+$rules = Yaml::parse(file_get_contents('./_data/rules/rules.yml'));
+
+$data = '<strong>Do\'s and don\'ts of the PHP Facebook group</strong><br>Dear members, programming is a lot about following rules with good practices and so is posting in this group. Let\'s take a moment and read the group rules so we can have useful and safe discussion environment. These rules are hosted on <a href="https://github.com/wwphp-fb/group-rules">GitHub</a> and presented on the <a href="http://wwphp-fb.github.io/rules.html">main group GitHub page</a>.';
+$data .= '<ul>';
+
+foreach ($rules as $rule) {
+    if(isset($rule['title']) && isset($rule['description'])) {
+        $data .= '<li><strong>' . $rule['title'] . '</strong><br>' . $rule['description'] . '</li>';
+    }
+}
+
+$data .= '</ul><br>';
+$data .= 'Enjoy your stay at the group and don\'t be shy to ask your question.';
+
+file_put_contents('./rules.html', $data, LOCK_EX);
